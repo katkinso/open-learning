ad951abae92be0e33a252877999df4b742b1ab14
katkinso <katyratkinson@yahoo.com>
2019-11-16 10:53:33 -0800

    Basic Scaffolding

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,5 +1,5 @@
 module.exports = {
     index(req, res, next){
-      res.render("static/index", {title: "Welcome to Bloccit"});
+      res.render("static/index", {title: "Welcome Static"});
     }
   }
