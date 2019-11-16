551224667f94853824352bed9a40b89f8d9d336d
katkinso <katyratkinson@yahoo.com>
2019-11-16 08:34:41 -0800

    3 Env Vars

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@
 const express = require("express");
 const app = express();
 
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 module.exports = app;
 
+appConfig.init();
 routeConfig.init(app);
--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
 const app = require("./app");
 const http = require("http");
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
 
 server.on("listening", () => {
-  console.log("server is listening for requests on port 3000");
+  console.log(`server is listening for requests on port ${server.address().port}`);
 });
